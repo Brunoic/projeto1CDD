1e512e7d452d1fac4b6f1a614c74d5007684b3ce
Veronur <luccadelchiaro@gmail.com>
2018-04-17 18:44:02 -0300

    .

---
+++ Z01---Grupo-H-master/Projetos/C-LogicaCombinacional/src/rtl/Not16.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity not16 is
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end not16;
+
+architecture rtl of not16 is
+begin 
+	q <= not (a);
+end rtl;
